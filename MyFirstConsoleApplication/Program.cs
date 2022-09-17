aaf0a27317034e613910888dfa38e7fa15a37cd8
angela.brunst <angela.hairstylist@gmail.com>
2022-09-16 19:28:39 -0700

    Added Person Class and Method

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -8,8 +8,20 @@ namespace MyFirstConsoleApplication
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            GetUserNameAndLocation();
+        }
+
+        private static void GetUserNameAndLocation()
+        {
+            Person person = new Person();
+            Console.WriteLine("What is your name?");
+            person.name = Console.ReadLine();
+            Console.WriteLine($"Hi {person.name}! Where are you from?");
+            person.location = Console.ReadLine();
+            Console.WriteLine($"I have never been to {person.location}. I bet it is nice. Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
