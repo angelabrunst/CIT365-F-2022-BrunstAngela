46379ee3de78f1905cb68d0e2f3a9e4a3c96524a
angela.brunst <angela.hairstylist@gmail.com>
2022-09-16 21:02:21 -0700

    Added Christmas Countdown and glazerapp

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -11,16 +11,43 @@ namespace MyFirstConsoleApplication
         static void Main()
         {
             GetUserNameAndLocation();
+
+            DateTime TodaysDate = DateTime.Now;
+            ChristmasCountdown(TodaysDate);
+
+            GlazerApp.RunExample();
+
+            Console.WriteLine("Press any key to end the program.");
+            Console.ReadKey();
         }
 
         private static void GetUserNameAndLocation()
         {
             Person person = new Person();
+
             Console.WriteLine("What is your name?");
             person.name = Console.ReadLine();
+
             Console.WriteLine($"Hi {person.name}! Where are you from?");
             person.location = Console.ReadLine();
-            Console.WriteLine($"I have never been to {person.location}. I bet it is nice. Press any key to continue...");
+
+            Console.WriteLine($"I have never been to {person.location}. I bet it is nice.");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        private static void ChristmasCountdown(DateTime thisDate)
+        {
+            Console.WriteLine($"Today's date is: {thisDate}");
+
+            DateTime christmasDate = Convert.ToDateTime("12/25/2022");
+            DateTime TodayDate = Convert.ToDateTime(thisDate);
+
+            double numberOfDays = (christmasDate - TodayDate).TotalDays;
+            int daysTilChristmas = (int)numberOfDays;
+
+            Console.WriteLine($"There are {daysTilChristmas} days until Christmas!");
+            Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
         }
     }
