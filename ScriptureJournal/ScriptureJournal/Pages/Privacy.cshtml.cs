a3cf8d5e9a8008ce55dbe999a089c48c38025b24
angela.brunst <angela.hairstylist@gmail.com>
2022-10-27 11:24:52 -0600

    Round 3 of creating a Scripture Journal

--- ScriptureJournal/ScriptureJournal/Pages/Privacy.cshtml.cs
+++ ScriptureJournal/ScriptureJournal/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyScriptureJournal.Pages
+namespace ScriptureJournal.Pages
 {
     public class PrivacyModel : PageModel
     {
